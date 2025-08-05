fcb62c8d9c2894228abd2b406bf5666cc46368f2
αѕтяσχ11 <devastro0010@gmail.com>
2025-08-05 21:07:42 +0100

    add: configurator

---
+++ config.ts
@@ -0,0 +1,14 @@
+export default {
+  user_name: "",
+  user_pn: "",
+  bot_name: "",
+  heroku: {
+    app_name: "",
+    api_key: ""
+  },
+  koyeb: {
+    app_name: "",
+    api_key: "",
+  },
+  branch: "core"
+}
